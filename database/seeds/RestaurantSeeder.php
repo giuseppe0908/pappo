ebef72494cec1e64fd2d0fc56b856c742ad81119
leonard0-b <leonardo.bongianni@gmail.com>
2021-06-24 17:14:17 +0200

    bozza homepage

--- database/seeds/RestaurantSeeder.php
+++ database/seeds/RestaurantSeeder.php
@@ -15,21 +15,29 @@ class RestaurantSeeder extends Seeder
         $restaurants = [
             [
                 'user_id' => '1',
-                'name' => 'Il Calabrese',
-                'description' => 'nduja',
-                'slug' => 'il-calabrese',
+                'name' => 'Pizzeria Vesuvio',
+                'description' => 'La pizzeria Vesuvio di Portici offre una pizza fragrante e leggera, seguendo i dettami della vera pizza
+                napoletana. Impasto morbido, ingredienti di prima scelta e prodotti DOP di origine campana. La
+                ricerca costante della qualità è la caratteristica principale della Pizzeria. Oggi, inoltre, l’offerta della
+                pizzeria Vesuvio si allarga alle pizze senza glutine e a quelle con farina integrale, per venire incontro
+                alle esigenze dei clienti.',
+                'slug' => 'pizzeria-vesuvio',
                 'address' => 'via Fasulla, 123',
                 'telephone_number' => '3333331298',
-                'photo' => 'img'
+                'photo' => './img/restaurants/pizza-restaurant.jpg'
             ],
             [
                 'user_id' => '2',
-                'name' => 'Il Dario',
-                'description' => 'è di Dario',
-                'slug' => 'il-dario',
+                'name' => 'Puerto Mexico',
+                'description' => 'In origine México al 104 nel rione Monti, divenuto poi México all’Aventino per circa un ventennio, oggi il
+                ristorante, a dispetto dell’età e della sua lunga storia, ha ancora voglia di innovarsi e divertire. Ne è nato un
+                locale tutto da scoprire e vivere, nell’atmosfera intima e nel gusto, naturalmente in pieno sabor de México. In origine México al 104 nel rione Monti, divenuto poi México all’Aventino per circa un ventennio, oggi il
+                ristorante, a dispetto dell’età e della sua lunga storia, ha ancora voglia di innovarsi e divertire. Ne è nato un
+                locale tutto da scoprire e vivere, nell’atmosfera intima e nel gusto, naturalmente in pieno sabor de México.',
+                'slug' => 'puerto-mexico',
                 'address' => 'via Dario, 17',
                 'telephone_number' => '33845190293',
-                'photo' => 'img'
+                'photo' => './img/restaurants/mexican-cuisine-4-540x360.jpg'
             ]
         ];
 
