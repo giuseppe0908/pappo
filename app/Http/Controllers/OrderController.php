71789a950e86ed69fe7bdd3cc312e803351906fb
Andrea Sabatini <sabatini.andrea@outlook.it>
2021-07-02 18:59:32 +0200

    Braintree Test

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -6,6 +6,11 @@ use App\Order;
 use App\getRestaurant;
 use Illuminate\Http\Request;
 
+use Braintree\Gateway;
+use App\Models\Models\Product;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Orders\OrderRequest;
+
 class OrderController extends Controller
 {
     /**
@@ -110,4 +115,43 @@ class OrderController extends Controller
     {
         //
     }
+
+    // Braintree
+        public function generate(Request $request,Gateway $gateway){
+            $token = $gateway->clientToken()->generate();
+
+            $data = [
+                'success' => true,
+                'token' => $token
+            ];
+
+            return response()->json($data,200);
+        }
+
+        public function makePayment(OrderRequest $request,Gateway $gateway){
+
+            // $product = Product::find($request->product);
+
+            $result = $gateway->transaction()->sale([
+                'amount' => $request->amount,
+                'paymentMethodNonce' => $request->token,
+                'options' => [
+                    'submitForSettlement' => true
+                ]
+            ]);
+
+            if($result->success){
+                $data = [
+                    'success' => true,
+                    'message' => "Transazione eseguita con Successo!"
+                ];
+                return response()->json($data,200);
+            }else{
+                $data = [
+                    'success' => false,
+                    'message' => "Transazione Fallita!!"
+                ];
+                return response()->json($data,401);
+            }
+        }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Braintree\Gateway;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->app->singleton(Gateway::class, function($app){
+            return new Gateway(
+                [
+                    'environment' => 'sandbox',
+                    'merchantId' => 'k45tr8ybd5qpnj5v',
+                    'publicKey' => '29m4jwnd446y5mw4',
+                    'privateKey' => 'b907720d5b8c46f2055715a8889e9fbb'
+                ]
+            );
+        });
     }
 }
+
